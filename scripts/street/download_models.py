529604ad04da0f7449aea337f6c99e4e1e946f27
Sevak Mardirosian <smardirosians@gmail.com>
2018-10-30 14:00:08 +0100

    fixing the import errors

--- scripts/street/download_models.py
+++ scripts/street/download_models.py
@@ -1,5 +1,12 @@
 import os
-from scripts.download_gdrive import *
+import sys
+from download_gdrive import download_file_from_google_drive
+from download_gdrive import unzip_file
+# from scripts.download_gdrive import *
+# from vid2vid.scripts.download_gdrive import download_file_from_google_drive
+# from . download_gdrive import unzip_file
+
+
 
 file_id = '1MKtImgtnGC28EPU7Nh9DfFpHW6okNVkl'
 chpt_path = './checkpoints/'
